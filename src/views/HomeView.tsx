c86f87176587266d660487077a52a885ad43cfa0
muco <mucorolle@gmail.com>
2020-09-05 23:18:18 +0200

    feat: Search element in search bar.

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useRef } from 'react';
 import {
     AutoComplete,
     Text,
@@ -9,11 +9,38 @@ import {
 } from '@geist-ui/react';
 import { DocumentHead, Container } from 'components';
 import { datasets } from 'mock/datasets';
+import { searchData } from 'mock/search';
 
 export const HomeView = () => {
-    const data = datasets;
     const [modalState, setModalState] = useState(false);
     const [path, setPath] = useState('');
+
+    const allOptions = searchData.map(({ name, path }) => {
+        return { label: name, value: name };
+    });
+
+    const [options, setOptions] = useState();
+    const [searching, setSearching] = useState(false);
+    const timer = useRef();
+
+    // triggered every time input
+    const searchHandler = (currentValue) => {
+        if (!currentValue) return setOptions([] as any);
+        setSearching(true);
+
+        const relatedOptions = allOptions.filter((item) =>
+            item.value.includes(currentValue)
+        );
+        // this is mock async request
+        // you can get data in any way
+        timer.current && clearTimeout(timer.current);
+        (timer as any).current = setTimeout(() => {
+            setOptions(relatedOptions as any);
+            setSearching(false);
+            clearTimeout(timer.current);
+        }, 1000);
+    };
+
     return (
         <Fragment>
             <DocumentHead />
@@ -22,26 +49,25 @@ export const HomeView = () => {
                     Burundiarxiv
                 </Text>
                 <AutoComplete
-                    searching={false}
+                    searching={searching}
                     placeholder="Search data here"
                     width="100%"
                     size="large"
-                >
-                    <AutoComplete.Searching>
-                        <span style={{ color: 'red' }}>waiting...</span>
-                    </AutoComplete.Searching>
-                </AutoComplete>
+                    options={options}
+                    onSearch={searchHandler}
+                />
 
                 <Text h2 style={{ margin: '30px 0', textAlign: 'center' }}>
                     Categories
                 </Text>
 
                 <div className="categories">
-                    {data.map(({ category_name, data }, i) => (
+                    {datasets.map(({ category_name, data }, i) => (
                         <div className="category" key={i}>
                             <Text h4>{category_name}</Text>
-                            {data.map(({ name, path }) => (
+                            {data.map(({ name, path }, i) => (
                                 <Text
+                                    key={i}
                                     onClick={() => {
                                         setModalState(true);
                                         setPath(path);
