f406f018e7c261d322b9a36e7c4b3e756b032f74
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-11-15 02:33:27 +0100

    Add better ex from samples

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -3,74 +3,27 @@ import { Layout } from 'components';
 import { Doughnut } from 'react-chartjs-2';
 
 export const DashboardView = () => {
-  const randomScalingFactor = () => Math.round(Math.random() * 100);
-  const window = {
-    chartColors: {
-      red: 'rgb(255, 99, 132)',
-      orange: 'rgb(255, 159, 64)',
-      yellow: 'rgb(255, 205, 86)',
-      green: 'rgb(75, 192, 192)',
-      blue: 'rgb(54, 162, 235)',
-      purple: 'rgb(153, 102, 255)',
-      grey: 'rgb(201, 203, 207)',
-    },
-  };
+  const randomNumber = () => Math.round(Math.random() * 100);
 
-  const config = {
-    type: 'doughnut',
-    data: {
-      datasets: [
-        {
-          data: [
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor(),
-          ],
-          backgroundColor: [
-            window.chartColors.red,
-            window.chartColors.orange,
-            window.chartColors.yellow,
-            window.chartColors.green,
-            window.chartColors.blue,
-          ],
-          label: 'Dataset 1',
-        },
-      ],
-      labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
-    },
-    options: {
-      responsive: true,
-      legend: {
-        position: 'top',
+  const data = {
+    labels: ['Red', 'Green', 'Yellow'],
+    datasets: [
+      {
+        data: [randomNumber(), randomNumber(), randomNumber()],
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
       },
-      title: {
-        display: true,
-        text: 'Chart.js Doughnut Chart',
-      },
-      animation: {
-        animateScale: true,
-        animateRotate: true,
-      },
-    },
+    ],
   };
   return (
     <Layout pageTitle="Dashboard">
       <Text h4 style={{ textAlign: 'center', marginTop: '30px' }}>
         POPULATION
       </Text>
-      <Note
-        type="success"
-        style={{
-          width: 'auto',
-          maxWidth: '400px',
-          margin: '0  auto 50px auto',
-        }}
-      >
-        Burundi's main demographic and socio-economic indicators!
-      </Note>
-      <Doughnut data={config.data} />
+      <Text h6 style={{ textAlign: 'center', marginTop: '30px' }}>
+        Burundi's main demographic and socio-economic indicators
+      </Text>
+      <Doughnut data={data} />
     </Layout>
   );
 };
