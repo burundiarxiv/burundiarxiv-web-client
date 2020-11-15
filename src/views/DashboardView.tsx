1fef234876e4243fc3deb3e21ba5b9ea371c3a7e
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-11-15 03:29:58 +0100

    Add more samples data

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,11 +1,11 @@
 import { Text, Grid, Card, Link, Image } from '@geist-ui/react';
 import { Layout } from 'components';
-import { Doughnut } from 'react-chartjs-2';
+import { Doughnut, Line, Bubble, Radar } from 'react-chartjs-2';
 
 export const DashboardView = () => {
-  const randomNumber = () => Math.round(Math.random() * 100);
+  const randomNumber = () => Math.round(Math.random() * 20);
 
-  const data = {
+  const doughnutData = {
     labels: ['Red', 'Green', 'Yellow'],
     datasets: [
       {
@@ -15,21 +15,113 @@ export const DashboardView = () => {
       },
     ],
   };
-  const MockItem = () => {
-    return <Card shadow style={{ width: '100%', height: '100px' }} />;
+
+  const radarData = {
+    labels: [
+      'Eating',
+      'Drinking',
+      'Sleeping',
+      'Designing',
+      'Coding',
+      'Cycling',
+      'Running',
+    ],
+    datasets: [
+      {
+        label: 'My First dataset',
+        backgroundColor: 'rgba(179,181,198,0.2)',
+        borderColor: 'rgba(179,181,198,1)',
+        pointBackgroundColor: 'rgba(179,181,198,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(179,181,198,1)',
+        data: [65, 59, 90, 81, 56, 55, 40],
+      },
+      {
+        label: 'My Second dataset',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        pointBackgroundColor: 'rgba(255,99,132,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(255,99,132,1)',
+        data: [28, 48, 40, 19, 96, 27, 100],
+      },
+    ],
   };
 
-  const Item = () => {
+  const bubbleData = {
+    labels: ['January'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [
+          { x: 10, y: 20, r: 5 },
+          { x: randomNumber(), y: randomNumber(), r: randomNumber() },
+          { x: randomNumber(), y: randomNumber(), r: randomNumber() },
+          { x: randomNumber(), y: randomNumber(), r: randomNumber() },
+          { x: randomNumber(), y: randomNumber(), r: randomNumber() },
+          { x: randomNumber(), y: randomNumber(), r: randomNumber() },
+          { x: randomNumber(), y: randomNumber(), r: randomNumber() },
+          { x: randomNumber(), y: randomNumber(), r: randomNumber() },
+          { x: randomNumber(), y: randomNumber(), r: randomNumber() },
+          { x: randomNumber(), y: randomNumber(), r: randomNumber() },
+          { x: randomNumber(), y: randomNumber(), r: randomNumber() },
+        ],
+      },
+    ],
+  };
+
+  const lineData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    ],
+  };
+  const Item = ({ Component, data }) => {
     return (
       <Card width="400px">
-        <Doughnut data={data} />
-        <Text
-          type="secondary"
-          small
-          style={{ textAlign: 'center', marginTop: '30px' }}
-        >
+        <Text h6 style={{ marginBottom: '0' }}>
           Burundi's main demographic and socio-economic indicators
         </Text>
+        <Component data={data} />
         <Card.Footer>
           <Link block target="_blank" href="#">
             Download data in CSV
@@ -39,16 +131,6 @@ export const DashboardView = () => {
     );
   };
 
-  const Item2 = () => {
-    return (
-      <>
-        <Text h6 style={{ textAlign: 'center', marginTop: '20px' }}>
-          Burundi's main demographic and socio-economic indicators
-        </Text>
-        <Doughnut data={data} />
-      </>
-    );
-  };
   return (
     <Layout pageTitle="Dashboard">
       <Text h4 style={{ textAlign: 'center' }}>
@@ -56,16 +138,16 @@ export const DashboardView = () => {
       </Text>
       <Grid.Container gap={5} justify="center">
         <Grid xs={24} md={12}>
-          <Item />
+          <Item Component={Doughnut} data={doughnutData} />
         </Grid>
         <Grid xs={24} md={12}>
-          <Item />
+          <Item Component={Line} data={lineData} />
         </Grid>
         <Grid xs={24} md={12}>
-          <Item />
+          <Item Component={Bubble} data={bubbleData} />
         </Grid>
         <Grid xs={24} md={12}>
-          <Item2 />
+          <Item Component={Radar} data={radarData} />
         </Grid>
       </Grid.Container>
     </Layout>
