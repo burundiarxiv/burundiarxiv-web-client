35d1e9c949e40ef6ee1a7cb119125653ad02e8a4
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-11-15 03:09:34 +0100

    Add elements in a grid

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,4 +1,4 @@
-import { Text, Note } from '@geist-ui/react';
+import { Text, Grid, Card, Link, Image } from '@geist-ui/react';
 import { Layout } from 'components';
 import { Doughnut } from 'react-chartjs-2';
 
@@ -9,21 +9,65 @@ export const DashboardView = () => {
     labels: ['Red', 'Green', 'Yellow'],
     datasets: [
       {
-        data: [randomNumber(), randomNumber(), randomNumber()],
+        data: [400, 300, 34],
         backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
         hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
       },
     ],
   };
+  const MockItem = () => {
+    return <Card shadow style={{ width: '100%', height: '100px' }} />;
+  };
+
+  const Item = () => {
+    return (
+      <Card width="400px">
+        <Doughnut data={data} />
+        <Text
+          type="secondary"
+          small
+          style={{ textAlign: 'center', marginTop: '30px' }}
+        >
+          Burundi's main demographic and socio-economic indicators
+        </Text>
+        <Card.Footer>
+          <Link block target="_blank" href="#">
+            Download data in CSV
+          </Link>
+        </Card.Footer>
+      </Card>
+    );
+  };
+
+  const Item2 = () => {
+    return (
+      <>
+        <Text h6 style={{ textAlign: 'center', marginTop: '20px' }}>
+          Burundi's main demographic and socio-economic indicators
+        </Text>
+        <Doughnut data={data} />
+      </>
+    );
+  };
   return (
     <Layout pageTitle="Dashboard">
-      <Text h4 style={{ textAlign: 'center', marginTop: '30px' }}>
+      <Text h4 style={{ textAlign: 'center' }}>
         POPULATION
       </Text>
-      <Text h6 style={{ textAlign: 'center', marginTop: '30px' }}>
-        Burundi's main demographic and socio-economic indicators
-      </Text>
-      <Doughnut data={data} />
+      <Grid.Container gap={5} justify="center">
+        <Grid xs={24} md={12}>
+          <Item />
+        </Grid>
+        <Grid xs={24} md={12}>
+          <Item />
+        </Grid>
+        <Grid xs={24} md={12}>
+          <Item />
+        </Grid>
+        <Grid xs={24} md={12}>
+          <Item2 />
+        </Grid>
+      </Grid.Container>
     </Layout>
   );
 };
