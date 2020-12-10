845074e1e0fd4ee422d3257e867fe9eae14a23b9
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-12-10 23:39:27 +0100

    Add config next.config.js

--- src/components/Header/Intro.tsx
+++ src/components/Header/Intro.tsx
@@ -4,19 +4,21 @@ import styled from 'styled-components/macro';
 export const Intro = () => {
   return (
     <StyledIntro>
-      <Text h1>Portail Open Data sur le Burundi</Text>
+      <Text h1>Plateforme ouverte des données sur le Burundi</Text>
       <Text em p type="secondary" size="1.25rem">
         Partageons, amélioreons et réutiliseons les données publiques
         burundaises.
       </Text>
       <Text p size="100%">
-        Burundiarxiv Open Data, initié et développé par un groupe d'ingénieurs,
-        statisticiens, data scientists Burundais, a pour objectif de fournir des
-        données ouvertes et réutilisables à propos du Burundi, et de centraliser
-        toutes les données valides à propos du Burundi. Il s'agit d'un travail
-        en cours d'élabaration. Pour le moment, nous centralisons les données
-        provenant des annuaires de l'ISTEEBU. Nous souhaitons proposer un outil
-        gratuit au service des citoyens, chercheurs, ONG et décideurs.
+        Burundiarxiv Open Data est un projet initié et développé par un groupe
+        d'ingénieurs, statisticiens, data scientists Burundais. Il a pour
+        objectif de fournir et de centraliser les données ouvertes et
+        réutilisables sur le Burundi. Il s'agit d'un travail en cours
+        d'élabaration. Pour le moment, nous mettons à disposition les données
+        provenant des annuaires de l'Institut de Statistiques et d'Etudes
+        Economiques du Burundi (ISTEEBU) et bientôt ceux de la Banque de la
+        République du Burundi (BRB). Nous souhaitons proposer un outil gratuit
+        au service des citoyens, chercheurs, ONG et décideurs.
       </Text>
     </StyledIntro>
   );
--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => (
   <NextLink href="/">
     <a>
       <Image
-        src="/logo.png?nf_resize=smartcrop&w=400&h=100"
+        src="https://raw.githubusercontent.com/burundiarxiv/open-data/preview-data/public/logo.png"
         alt="me"
         width="400"
         height="100"
