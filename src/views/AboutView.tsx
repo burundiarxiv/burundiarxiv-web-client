23c44d4d8a889f032f6cfe3abaf34bea544d7320
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-12-19 18:11:29 +0100

    add dashboard section

--- src/views/AboutView.tsx
+++ src/views/AboutView.tsx
@@ -1,14 +1,22 @@
 import styled from 'styled-components/macro';
-import { Text, Divider, Link } from '@geist-ui/react';
+import { Text, Divider, Link, Grid, Card } from '@geist-ui/react';
 
 import { Layout } from 'components';
 
 export const AboutView = () => {
+  const DashboardItem = ({ size, description }) => {
+    return (
+      <Card shadow hoverable style={{ width: '100%', height: '100px' }}>
+        <h4>{size}</h4>
+        <p>{description}</p>
+      </Card>
+    );
+  };
   return (
     <StyledAboutView>
       <Layout pageTitle="About">
         <div>
-          <Text h3>Sources</Text>
+          <Text h3>Publications</Text>
           <Text p>
             Ce projet n'aurait jamais vu le jour sans la publication publique
             des données de :
@@ -53,11 +61,29 @@ export const AboutView = () => {
             <li>Trésor Sindihebura</li>
           </ul>
           <Divider />
-          <Text p>
-            Pour contribuer, publier, corriger ou mettre à jour les données,
-            vous pouvez nous contacter à :
-            <Text em> data(at)burundiarxiv(point)org</Text>
-          </Text>
+          <div>
+            <Text h3>Tableau de bord</Text>
+            <Grid.Container gap={2} justify="center">
+              <Grid xs={24} sm={12} md={8}>
+                <DashboardItem size={290} description="Jeux de données" />
+              </Grid>
+              <Grid xs={24} sm={12} md={8}>
+                <DashboardItem size={70} description="Sources" />
+              </Grid>
+              <Grid xs={24} sm={12} md={8}>
+                <DashboardItem size={20} description="Domaines" />
+              </Grid>
+            </Grid.Container>
+          </div>
+          <Divider />
+          <div>
+            <Text h3>Contact</Text>
+            <Text p>
+              Pour contribuer, publier, corriger ou mettre à jour les données,
+              vous pouvez nous contacter à :
+              <Text em> data(at)burundiarxiv(point)org</Text>
+            </Text>
+          </div>
           <Divider />
         </div>
       </Layout>
